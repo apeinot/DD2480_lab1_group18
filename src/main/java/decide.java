f2a7ed37e5dd0820ac215b6180f3707a7e96f03b
Alexandre PEINOT <alexandre.peinot@eyesnap.fr>
2019-01-23 14:02:33 +0100

    Add basic format in main and test files

--- src/main/java/decide.java
+++ src/main/java/decide.java
@@ -1,1 +1,9 @@
 //Main file of Decide programm
+
+class Decide {
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+
+}
--- src/test/java/decideTest.java
+++ src/test/java/decideTest.java
@@ -1,1 +1,12 @@
 //Test cases of decide functionnality
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DecideTest {
+  
+    @Test
+    public void testTrue() {
+        assertEquals(true, true);
+    }
+}
