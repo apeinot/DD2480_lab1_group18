ff59a4a86eb517d0974e937f92cc66c0e9aea3c0
Emil Elmarsson <emilelm@kth.se>
2019-01-24 22:55:24 +0100

    fix #15: Added functionality and tests for LIC10.
    Assesses if there exists at least one set of three data points separated by exactly E_PTS and F_PTS consecutive intervening points, respectively, that are the vertices of a triangle with area greater than AREA1 (PARAMETERS.AREA1).

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -50,11 +50,48 @@ public class DecideTest {
 
 
     // Test of the LIC number 0 in a case where the condition should not be triggered (false)
-    
+
 	system.NUMPOINTS = 5;
         system.X = new double[] {0, 20, 2, 0, 12};
         system.Y = new double[] {1, 10, 3, 0, 0};
         assertEquals(system.LIC0(), false);
+    }
+
+    @Test
+    public void testLIC3(){
+        Decide system = new Decide();
+        /*
+        Test with area less than AREA1 (200 square units), should be false.
+        The triangle points are (0,0), (3,0) and (3,4), which creates a
+        triangle with an area of 6 square units. Separated by (E_PTS = 6) and
+        (F_PTS = 9) points.
+        */
+        system.NUMPOINTS = 18;
+        system.X = new double[] {0,   1,2,3,4,5,6,   3,   1,2,3,4,5,6,7,8,9,   3};
+        system.Y = new double[] {0,   1,2,3,4,5,6,   0,   1,2,3,4,5,6,7,8,9,   4};
+        assertEquals(system.LIC10(), false);
+
+        /*
+        Test with area more than AREA1, should be true. The triangle is (0,0),
+        (21,0) and (21,21) which creates a triangle of 220.5 square units. This
+        is greater than AREA1 (220 square units).
+        */
+        system.NUMPOINTS = 18;
+        system.PARAMETERS.AREA1 = 220;
+        system.X = new double[] {0,   1,2,3,4,5,6,   21,   1,2,3,4,5,6,7,8,9,   21};
+        system.Y = new double[] {0,   1,2,3,4,5,6,   0,   1,2,3,4,5,6,7,8,9,   21};
+        assertEquals(system.LIC10(), true);
+
+        // Now the area is higher than that of the triangle, so this should be false.
+        system.PARAMETERS.AREA1 = 221;
+        assertEquals(system.LIC10(), false);
+
+        // Not enough points, should be false. E_PTS + F_PTS = 6 + 9 = 15 > NUMPOINTS - 3 = 14.
+        system.NUMPOINTS = 17;
+        system.PARAMETERS.AREA1 = 200;
+        system.X = new double[] {0,   1,2,3,4,5,6,   21,   1,2,3,4,5,6,7,8,   21};
+        system.Y = new double[] {0,   1,2,3,4,5,6,   0,   1,2,3,4,5,6,7,8,   21};
+        assertEquals(system.LIC10(), false);
     }
 
 }
